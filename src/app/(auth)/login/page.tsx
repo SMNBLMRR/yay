41386dcafc088f141d2eba245a301d7ee5ddc6b5
Simone Marrandino <simoneblog9991@gmail.com>
2024-01-12 10:55:03 +0100

    update

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -24,66 +24,72 @@ const Login: FunctionComponent<LoginProps> = () => {
 
   return (
     <>
-      {/* <Header /> */}
-      <div className="h-[calc(100vh-64px)] w-full flex justify-center items-center">
-        <div className="flex flex-col w-1/4 m-auto">
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-            <div className="flex flex-col">
-              <Input
-                label="Email"
-                placeholder="Insert you email addresss..."
-                classNames={{
-                  label: [
-                    "active:!text-black",
-                    "dark:group-focus-within:!text-red-600",
-                    "text-black",
-                    "dark:!text-black",
-                    "dark:group-data-[focus-within=true]:!text-black",
-                  ],
-                  input: [
-                    "bg-transparent",
-                    "dark:!text-red-900",
-                    "text-red-900",
-                    "placeholder:text-black",
-                  ],
-                  innerWrapper: "bg-transparent",
-                  inputWrapper: [
-                    "bg-[#b9b4ff]",
-                    "dark:hover:bg-[#b9b4ff]",
-                    "dark:focus-within:bg-[#b9b4ff]",
-                  ],
-                }}
-                {...register("email")}
-              />
-            </div>
-            <Button
-              className="mt-5 border-[#b9b4ff] text-[#b9b4ff]"
-              variant="bordered"
-            >
-              Continue with email
-            </Button>
-            {errors.email && <span>This field is required</span>}
-          </form>
-          <Divider className="my-10" />
+    {/* <Header /> */}
+    <div className="h-[calc(100vh-64px)] w-full flex justify-center items-center">
+      <div className="flex flex-col w-1/4 m-auto">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+          <div className="flex flex-col">
+            <Input
+              label="Email"
+              placeholder="Insert you email addresss..."
+              classNames={{
+                label: [
+                  "active:!text-black",
+                  "dark:group-focus-within:!text-red-600",
+                  "text-black",
+                  "dark:!text-black",
+                  "dark:group-data-[focus-within=true]:!text-black",
+                ],
+                input: [
+                  "bg-transparent",
+                  "dark:!text-red-900",
+                  "text-red-900",
+                  "placeholder:text-black",
+                ],
+                innerWrapper: "bg-transparent",
+                inputWrapper: [
+                  "bg-[#b9b4ff]",
+                  "dark:hover:bg-[#b9b4ff]",
+                  "dark:focus-within:bg-[#b9b4ff]",
+                ],
+              }}
+              {...register("email")}
+            />
+          </div>
           <Button
-            onClick={() => {console.log("ciao")}}
-            className="mt-2"
-            color="danger"
+            className="mt-5 border-[#b9b4ff] text-[#b9b4ff]"
             variant="bordered"
-            endContent={<FcGoogle size={20} />}
           >
-            Continue with Google
+            Continue with email
           </Button>
-          <Button
-            className="mt-5"
-            color="danger"
-            variant="bordered"
-            endContent={<FaApple size={20} />}
-          >
-            Continue with Apple
-          </Button>
-        </div>
+          {errors.email && <span>This field is required</span>}
+        </form>
+        <Divider className="my-10" />
+        <Button
+          onClick={() => {
+            signIn("email", {
+              redirect:false,
+              email:"semn@gft.com"
+              });
+            } 
+          }
+          className="mt-2"
+          color="danger"
+          variant="bordered"
+          endContent={<FcGoogle size={20} />}
+        >
+          Continue with Google
+        </Button>
+        <Button
+          className="mt-5"
+          color="danger"
+          variant="bordered"
+          endContent={<FaApple size={20} />}
+        >
+          Continue with Apple
+        </Button>
       </div>
+    </div>
     </>
   );
 };
