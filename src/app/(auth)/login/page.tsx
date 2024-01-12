afcce29773f6d5e331eda0a36f55d94922c11074
Simone Marrandino <simoneblog9991@gmail.com>
2024-01-12 10:35:12 +0100

    udapte

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -24,75 +24,70 @@ const Login: FunctionComponent<LoginProps> = () => {
 
   return (
     <>
-    {/* <Header /> */}
-    <div className="h-[calc(100vh-64px)] w-full flex justify-center items-center">
-      <div className="flex flex-col w-1/4 m-auto">
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-          <div className="flex flex-col">
-            <Input
-              label="Email"
-              placeholder="Insert you email addresss..."
-              classNames={{
-                label: [
-                  "active:!text-black",
-                  "dark:group-focus-within:!text-red-600",
-                  "text-black",
-                  "dark:!text-black",
-                  "dark:group-data-[focus-within=true]:!text-black",
-                ],
-                input: [
-                  "bg-transparent",
-                  "dark:!text-red-900",
-                  "text-red-900",
-                  "placeholder:text-black",
-                ],
-                innerWrapper: "bg-transparent",
-                inputWrapper: [
-                  "bg-[#b9b4ff]",
-                  "dark:hover:bg-[#b9b4ff]",
-                  "dark:focus-within:bg-[#b9b4ff]",
-                ],
-              }}
-              {...register("email")}
-            />
-          </div>
+      {/* <Header /> */}
+      <div className="h-[calc(100vh-64px)] w-full flex justify-center items-center">
+        <div className="flex flex-col w-1/4 m-auto">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+            <div className="flex flex-col">
+              <Input
+                label="Email"
+                placeholder="Insert you email addresss..."
+                classNames={{
+                  label: [
+                    "active:!text-black",
+                    "dark:group-focus-within:!text-red-600",
+                    "text-black",
+                    "dark:!text-black",
+                    "dark:group-data-[focus-within=true]:!text-black",
+                  ],
+                  input: [
+                    "bg-transparent",
+                    "dark:!text-red-900",
+                    "text-red-900",
+                    "placeholder:text-black",
+                  ],
+                  innerWrapper: "bg-transparent",
+                  inputWrapper: [
+                    "bg-[#b9b4ff]",
+                    "dark:hover:bg-[#b9b4ff]",
+                    "dark:focus-within:bg-[#b9b4ff]",
+                  ],
+                }}
+                {...register("email")}
+              />
+            </div>
+            <Button
+              className="mt-5 border-[#b9b4ff] text-[#b9b4ff]"
+              variant="bordered"
+            >
+              Continue with email
+            </Button>
+            {errors.email && <span>This field is required</span>}
+          </form>
+          <Divider className="my-10" />
           <Button
-            className="mt-5 border-[#b9b4ff] text-[#b9b4ff]"
-            variant="bordered"
-          >
-            Continue with email
-          </Button>
-          {errors.email && <span>This field is required</span>}
-        </form>
-        <Divider className="my-10" />
-        {/* <Button
-          onClick={async () => {
-            try {
+            onClick={() => {
               signIn("google", {
                 callbackUrl: "/app/today",
               });
-            } catch (e) {
-              console.log(e);
-              console.error("Error...");
-            }
-          }}
-          className="mt-2"
-          color="danger"
-          variant="bordered"
-          endContent={<FcGoogle size={20} />}
-        >
-          Continue with Google
-        </Button> */}
-        <Button
-          className="mt-5"
-          color="danger"
-          variant="bordered"
-          endContent={<FaApple size={20} />}
-        >
-          Continue with Apple
-        </Button>
+            }}
+            className="mt-2"
+            color="danger"
+            variant="bordered"
+            endContent={<FcGoogle size={20} />}
+          >
+            Continue with Google
+          </Button>
+          <Button
+            className="mt-5"
+            color="danger"
+            variant="bordered"
+            endContent={<FaApple size={20} />}
+          >
+            Continue with Apple
+          </Button>
+        </div>
       </div>
-    </div>
     </>
   );
 };
